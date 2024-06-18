297d3373e97bf378614043a84f51652820f1ecd7
archit381 <architojha420@gmail.com>
2024-06-18 18:56:41 +0530

    Implementing Detailed View for each item

---
+++ Core/Components/CarouselView.swift
@@ -0,0 +1,18 @@
+//
+//  CarouselView.swift
+//  AirBnb-Clone
+//
+//  Created by Anushka Mishra on 18/06/24.
+//
+
+import SwiftUI
+
+struct CarouselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarouselView()
+}
---
+++ Core/Explore/SearchFilterView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchFilterView.swift
+//  AirBnb-Clone
+//
+//  Created by Anushka Mishra on 18/06/24.
+//
+
+import SwiftUI
+
+struct SearchFilterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchFilterView()
+}
