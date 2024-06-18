dce1557746bfec85d9ce1e37eea2e7925e247697
archit381 <architojha420@gmail.com>
2024-06-18 17:49:47 +0530

    first

---
+++ AirBnb_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBnb_CloneApp.swift
+//  AirBnb-Clone
+//
+//  Created by Anushka Mishra on 18/06/24.
+//
+
+import SwiftUI
+
+@main
+struct AirBnb_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Core/Explore/ExploreView.swift
@@ -0,0 +1,24 @@
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        NavigationStack{
+            ScrollView{
+                LazyVStack(spacing:32){
+                    ForEach(0 ... 10, id: \.self){ listing in
+                        ListingItemView()
+                            .frame(height: 430)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                    }
+                }
+                .padding()
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    ExploreView()
+}
